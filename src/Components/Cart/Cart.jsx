cafa72a9428ac230dee609435d514b8eaad5c6bb
Muinul-Islam <muinul1022@gmail.com>
2023-09-14 22:01:47 +0600

    dynamically cards name can be added to the cart section

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 import "./Cart.css";
 const Cart = ({ courses }) => {
   console.log(courses);
   return (
     <div>
-      <h1>Cart</h1>
+      <h3>Credit Hour Remaining 7 hr</h3>
+      <hr />
+      <h2>Course Name</h2>
+      {courses.map((course, index) => (
+        <li className="list-item" key={index}>
+          {index + 1} {course.name}
+        </li>
+      ))}
+      <hr />
+      <h4>Total Credit Hour : 13</h4>
+      <hr />
+      <h4>Total Price : 48000 USD</h4>
     </div>
   );
 };
--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -3,22 +3,26 @@ import { useEffect } from "react";
 import Cart from "../Cart/Cart";
 import "./Courses.css";
 import { useState } from "react";
+import { BsBook } from "react-icons/bs";
 const Courses = () => {
+  const [allCourses, setAllCourses] = useState([]);
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
     fetch("./data.json")
       .then((res) => res.json())
-      .then((data) => setCourses(data));
+      .then((data) => setAllCourses(data));
   }, []);
-  const handleClicked = (course) => {};
+  const handleClicked = (course) => {
+    setCourses([...courses, course]);
+  };
 
   return (
     <div>
       <div className="container">
         <div className="courses">
           <div className="card-container">
-            {courses.map((course) => (
+            {allCourses.map((course) => (
               <div className="card">
                 <img className="card-img" src={course.img} alt="" />
                 <h3>{course.name}</h3>
@@ -26,7 +30,9 @@ const Courses = () => {
                 <div className="info">
                   <p>$ Price: {course.price}</p>
                   <div>
-                    <p>Credit: {course.credit_hours}hr</p>
+                    <p className="credit-icon">
+                      <BsBook></BsBook>Credit: {course.credit_hours}hr
+                    </p>
                   </div>
                 </div>
                 <button onClick={() => handleClicked(course)} className="btn">
